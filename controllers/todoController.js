c7c1439517fd75b10ba86c5118888050cdc6b5d5
developer-junaid <qjunaid623@gmail.com>
2021-05-17 09:55:22 +0500

    Added Dummy Data from controller to view
    NEXT: Add and Delete Functionality

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,9 +1,16 @@
+// Data
+let data = [
+  { item: "get milk" },
+  { item: "take online classes" },
+  { item: "create project" },
+];
+
 module.exports = function (app) {
   // Todo Logic
 
   app.get("/todo", (req, res) => {
     // Get logic
-    res.render("todo"); // render todo
+    res.render("todo", { todos: data }); // render todo and pass data
   });
 
   app.get("/todo", (req, res) => {
